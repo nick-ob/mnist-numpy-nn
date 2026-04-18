bb335ecce56765f089bddfe64f70fdae4ace63cf
nick-ob <obriennick09@gmail.com>
2026-04-18 15:45:01 +0200

    Added softmax activation logic

--- src/activations.py
+++ src/activations.py
@@ -60,3 +60,54 @@ class Sigmoid:
         # add derivative of the sigmoid to delta
         sigmoid = self.__sigmoid(self.__x)
         return delta * (sigmoid * (1 - sigmoid))
+
+class Softmax:
+    """A class representing the softmax activation in a neural network.
+
+    In this code implementation, activation functions are logically implemented as a seperate layer in the network.
+    Thus, activation functions have the same public functions as the Layer class.
+    """
+    def __softmax(self, x: np.ndarray) -> np.ndarray:
+        """Softmax function. Squish any values into the range of 0-1, representing a probability. So sum of these = 1.
+
+        Args:
+            x: The input to squish to probabilities.
+
+        Returns:
+            np.ndarray: The squished result.
+        """
+        # the input is normalised (subtract each value by the maximum value) to avoid numerical overflow in the softmax function
+        # this works since the softmax function is shift-invariant
+        x = x - np.max(x, axis=1, keepdims=True) # shape (batches, nodes)
+
+        exp = np.exp(x)
+        return exp / np.sum(exp, axis=1, keepdims=True)
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Compute softmax and forward it to the next layer.
+
+        Args:
+            x: The input from the previous layer.
+
+        Returns:
+            np.ndarray: The input for the upcoming Layer.
+        """
+        return self.__softmax(x)
+
+
+    def backward(self, delta: np.ndarray, _) -> np.ndarray:
+        """Pass on delta to the previous Layer.
+
+        Since our softmax layer is always the last layer in our network, its delta is the derivative of the loss
+        w.r.t. the output of the network, which is calculated in the Loss class. This is done so that this class can
+        be called in the same way as the Layer class.
+
+        Args:
+            delta: The derivative of the loss w.r.t. the output of this layer.
+            _: A placeholder to be able to use this class the same way as the Layer class.
+
+        Returns:
+            np.ndarray: Delta, the derivative of the loss w.r.t. the output of the previous layer.
+        """
+        # simply pass on delta recieved from the Loss class
+        return delta
